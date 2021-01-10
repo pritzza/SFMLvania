7519ab3d74918b3774ac25042200be44ada6a4b0
Jonathan <jonathanzakharov@gmail.com>
2021-01-10 00:22:57 -0800

    Revert "Revert "fixed""
    This reverts commit 4c614b19b657d27034f0c6021cdf6955613d34b3.

--- src/src/events/Event.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Event	// all event should be dynamically constructed as an argument for the EventHandler's .addEvent() method
-{
-public:
-	void virtual eventAction() = 0;	// all events are dynamically allocated and should be passed into the EventHandler, processed, then deleted all by the EventHandler
-};
